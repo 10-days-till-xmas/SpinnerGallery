a5e16c47758d2bebe4607d2cdc7f7e44168fb33d
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-12-04 00:57:04 +0000

    Initial commit

---
+++ SpinnerGallery/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Hosting;
+using RazorConsole.Core;
+using SpinnerGallery;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+var hostBuilder = Host.CreateDefaultBuilder()
+                      .UseRazorConsole<Gallery>();
+var host = hostBuilder.Build();
+await host.RunAsync();
